eafa44f8d0231e974dd9754863da3e5f73d222fe
ValentinoChandra <valentino.chandra@itbss.ac.id>
2023-06-01 21:00:59 +0700

    commit

---
+++ welcome.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title> Welcome </title>
+
+        <link rel="stylesheet" href="css/welcome.css">
+
+    </head>
+    <body>
+        <div class="container">
+            <div class="content">
+                <h3> Hi, <span> kawan </span></h3>
+                <h1> Selamat datang di Kantin Mama </h3>
+                <a href="home_page.php" class="btn"> Masuk </a>
+                <a href="logout.php" class="btn"> Keluar </a>
+            </div>
+        </div>
+    </body>
+</html>
